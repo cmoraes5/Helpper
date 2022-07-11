5d1f3bd8e1ef3afc03b3e79956218408527554f1
cmoraes5 <moraescaio0502@gmail.com>
2022-07-11 12:47:09 -0300

    we need to fix this bug

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,11 @@ import express from 'express'
 
 const app = express();
 
-app.post('/users', (req, res) => {
-    return res.send('Hello World 2'); 
+app.get('/feedbacks', (req, res) => {
+    return res.send('Hello World'); 
 })
 
 app.listen(3333, () => {
     console.log('HTTP server running!');
 });
 
-// SQLite
